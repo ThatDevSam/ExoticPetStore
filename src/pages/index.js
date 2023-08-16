0ef4ec66df9b63d1fba6403b26f1184034d4545d
ThatDevSam <webdev@samcomp>
2023-08-16 13:12:49 -0400

    Added an export for the Products page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Home from './Home'
 import SharedLayout from './SharedLayout'
 import SingleProduct from './SingleProduct'
 import Error from './Error'
+import Products from './Products'
 
-export { Home, SharedLayout, SingleProduct, Error}
+export { Home, SharedLayout, SingleProduct, Error, Products}
