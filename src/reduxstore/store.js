db6613e2a74eca900ac65a67d30cd9354eb9f9e7
ThatDevSam <webdev@samcomp>
2023-08-23 16:36:12 -0400

    Added in the searchReducer to track the user's current search parameters.

--- src/reduxstore/store.js
+++ src/reduxstore/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './apiSlice'
+import searchReducer from './searchSlice'
 
 export default configureStore({
   reducer: {
+    search: searchReducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
   middleware: getDefaultMiddleware =>
